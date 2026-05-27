8b187bd48dc79450a10ba7f7685d9b46e9f5c663
princerijal <127306646+princerijal@users.noreply.github.com>
2026-05-27 16:13:21 +0100

    Create stored procedure for populating bronze tables
    This stored procedure populates tables in the bronze layer using BULK INSERT after truncating existing data.

---
+++ scripts/bronze/create_stored_procedure.sql
@@ -0,0 +1,106 @@
+/*
+
+PURPOSE:
+    The purpose of this script is to populate all of the tables created in bronze layer
+I am using BULK INSERT function
+
+
+WARNING:
+
+Check the table names and run this script carefully as the script truncates the table first and then only populates them
+
+
+*/
+
+CREATE OR ALTER Procedure bronze.load_bronze AS 
+	BEGIN
+
+		-- THE TABLE IS TRUBNCATED so that, even if the script is run, the data wont be added 2 times
+		TRUNCATE TABLE bronze.crm_cust_info;
+
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\SRijal\Desktop\SQL\DataWarehouse\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+
+	
+
+
+		TRUNCATE TABLE Bronze.crm_prod_info
+
+		BULK INSERT bronze.crm_prod_info
+			FROM 'C:\Users\SRijal\Desktop\SQL\DataWarehouse\datasets\source_crm\prd_info.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+				);
+
+		
+
+
+
+
+		
+
+		TRUNCATE TABLE Bronze.crm_sales_details
+		BULK INSERT Bronze.crm_sales_details
+			FROM 'C:\Users\SRijal\Desktop\SQL\DataWarehouse\datasets\source_crm\sales_details.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
+
+		
+
+
+
+
+
+
+		TRUNCATE TABLE Bronze.erp_cat_g1v2
+		BULK INSERT Bronze.erp_cat_g1V2
+		FROM 'C:\Users\SRijal\Desktop\SQL\DataWarehouse\datasets\source_erp\px_cat_g1v2.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+
+
+
+
+
+		TRUNCATE TABLE Bronze.erp_cust_az12
+
+		BULK INSERT Bronze.erp_cust_az12
+			FROM 'C:\Users\SRijal\Desktop\SQL\DataWarehouse\datasets\source_erp\cust_az12.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
+
+	
+
+
+
+		TRUNCATE TABLE Bronze.erp_loc_a101
+
+		BULK INSERT Bronze.erp_loc_a101
+			FROM 'C:\Users\SRijal\Desktop\SQL\DataWarehouse\datasets\source_erp\loc_a101.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
+
+		
+
+	END
